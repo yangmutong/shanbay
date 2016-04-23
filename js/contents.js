86f07120553646a8917faa1408526d295e0685f8
yangmutong <985777876@qq.com>
2016-04-23 21:26:28 +0800

    add ajax fetch definition and clear the other part of the page

--- js/contents.js
+++ js/contents.js
@@ -3,15 +3,51 @@ var selectString;
 var $article = $("#article h1,figure,p");
 
 
-window.onmouseup = function(){
+window.addEventListener("mouseup",function(event){
     selectedWord = window.getSelection();
     if(selectedWord.anchorOffset != selectedWord.extentOffset){
         selectString = selectedWord.toString();
+        getDefinition(selectString);
+        console.log(selectString);
     }
+},false);
+
+function getDefinition(word){
+	$.ajax({
+		type:"GET",
+		url:"https://api.shanbay.com/bdc/search/?word=" + word,
+		dataType:"json",
+		success:function(data){
+			var d = JSON.stringify(data)
+			console.log(d);
+			var t = JSON.parse(d);
+			console.log(t);
+			console.log(t.data.definition);
+		},
+		error:function(error){
+			console.log(error);
+		}
+	});
 }
+
 
 function createHTML(){
 	$("body").html($article);
 }
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    if (request.order == "clear"){
+    	//这里精简页面并分页
+    	createHTML();
+      	sendResponse({result: "success"});
+    }
+    else if (request.order == "reload") {
+    	//这里将页面恢复原状
+    	location.reload();
+    	sendResponse({result: "success"});
+    }
+  });
 
 
--- js/shanbay.js
+++ js/shanbay.js
@@ -1,21 +1,20 @@
 var $btn = $("#btn");
-var status = localStorage.status;
-status = status?status:"closed";
-console.log(status);
-init();
+var currentId;
+var status;
 $btn.click(function(event) {
-	// body...
 	if (event.target.id == "btn") {
 		if($(this).html() == "开启"){
 			$(this).html("关闭");
-			localStorage.status = "open";
+			localStorage["" + currentId] = "open";
 			$(this).addClass("changed");
+			sendMessage(currentId,{order:"clear"});
 			//切换页面，这里清除页面格式；
 		}
 		else{
 			$(this).html("开启");
-			localStorage.status = "closed";
+			localStorage["" + currentId] = "closed";
 			$(this).removeClass("changed");
+			sendMessage(currentId,{order:"reload"});
 			//切换页面，将页面还原为原来的格式；
 		}
 	}
@@ -32,13 +31,29 @@ function setBtn(){
 	}
 }
 
-function init(){
+function getCurrentTabId(callback){
+	chrome.tabs.query({
+	active:true,
+	currentWindow:true,
+	},function(tabsArr){
+		callback(tabsArr[0].id);
+	});
+
+}
+
+function setStatus(id){
+	currentId = id;
+	status = localStorage["" + currentId];
+	status = status?status:"closed";
 	setBtn();
 }
 
-chrome.tabs.query({
-	active:true,
-	currentWindow:true,
-},function(tabsArr){
-	console.log(tabsArr);
-});
+getCurrentTabId(setStatus);
+
+
+function sendMessage(tabId,message){
+	chrome.tabs.sendMessage(tabId,message,function(response){
+		console.log(response);
+	})	
+}
+
